62f192141084c200fb2646821088d1cf7d5b7624
F1RST1 <47220608+F1RST1@users.noreply.github.com>
2024-07-23 08:25:35 +0000

    [edit] change value to pointer to save memory

--- main.c
+++ main.c
@@ -5,13 +5,13 @@ void __caller(aslot_callback_t slot)
 {
     static int x = 70;
     static int x2= 50;
-    slot(0, 89, 48);
+    slot(0, &x, &x2);
 }
 
-void callback_on_happened(int ignore, int x, int x2)
+void callback_on_happened(int ignore, int *x, int *x2)
 {
-    printf("recv:%d\r\n", x);
-    printf("recv:%d\r\n", x2);
+    printf("recv:%d\r\n", *x);
+    printf("recv:%d\r\n", *x2);
 }
 
 int main(int argc, char const *argv[])
